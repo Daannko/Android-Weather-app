b15a2f008fb2e149d3cdea25c6c0ea1594f77a0a
Daniel <228776@edu.p.lodz.pl>
2022-05-11 22:44:53 +0200

    ViewPagger dodany TODO: dodaj sciaganie danych z internetu

--- app/src/main/java/com/example/aplikacjapogodowaandroid/MainActivity.java
+++ app/src/main/java/com/example/aplikacjapogodowaandroid/MainActivity.java
@@ -2,32 +2,36 @@ package com.example.aplikacjapogodowaandroid;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.aplikacjapogodowaandroid.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private String url = "api.openweathermap.org/data/2.5/forecast";
+    private String appID = "135ed2b36525fa416686f568e19c22cd";
+    DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +39,37 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         ViewPager2 viewPager2 = findViewById(R.id.ViewPager);
+
         viewPager2.setAdapter(
                 new SampleAdapter(this)
         );
+        viewPager2.setCurrentItem(1);
+    }
+
+    public void GetWeatherDetail(View view){
+        String tempUrl = "";
+        String city ="Warszawa";
+        if (city.equals("")) {
+            System.out.println("Puste miasto");
+        }
+        else
+        {
+            tempUrl = url + "?q=" + city + "&appid"+ appID;
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("response",response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString().trim(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+
     }
 
     class SampleAdapter extends FragmentStateAdapter
@@ -61,18 +93,20 @@ public class MainActivity extends AppCompatActivity {
             switch (position)
             {
                 case 0:
-                    return new MainPage("główna strona");
+                    return new ChoseCity();
                 case 1:
-                    return new SecondPage("druga strona");
+                    return new MainPage("główna strona");
                 case 2:
-                    return new MainPage("trzecia strona");
+                    return new SecondPage("druga strona");
+                case 3:
+                    return new ThirdPage("trzecia strona");
             }
             return new MainPage("ERROR");
         }
 
         @Override
         public int getItemCount() {
-            return 3;
+            return 4;
         }
     }
 }
--- app/src/main/java/com/example/aplikacjapogodowaandroid/ThirdPage.java
+++ app/src/main/java/com/example/aplikacjapogodowaandroid/ThirdPage.java
@@ -9,20 +9,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class SecondPage extends Fragment {
+public class ThirdPage extends Fragment {
 
 
     String title;
 
-    public SecondPage(String title) {
+    public ThirdPage(String title) {
         this.title = title;
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_second_page, container, false);
-        TextView sampleTextView = root.findViewById(R.id.TextViewSecoundFragment);
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_third_page, container, false);
+        TextView sampleTextView = root.findViewById(R.id.textViewSampleText);
         sampleTextView.setText(this.title);
         return root;
     }
